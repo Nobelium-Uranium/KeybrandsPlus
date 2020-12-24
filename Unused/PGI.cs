4321f4e1dcdcd431d8281fa0e7a3ad6fb9f93408
Chem <minecartanimation@gmail.com>
2020-12-24 04:08:26 -0700

    null

---
+++ Unused/PGI.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace KeybrandsPlus.Unused
+{
+    class PGI : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Placeholder Object");
+            Tooltip.SetDefault("Perfectly generic, or is it generically perfect?");
+        }
+    }
+}
