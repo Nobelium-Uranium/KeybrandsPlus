73aeb44a55656e25098519f63127d21f084f33b0
Chem <minecartanimation@gmail.com>
2020-12-30 18:47:03 -0700

    Draconic Flare now explodes when in proximity of an enemy

--- Projectiles/DraconicFlareBolt.cs
+++ Projectiles/DraconicFlareBolt.cs
@@ -1,5 +1,6 @@
 ﻿using KeybrandsPlus.Helpers;
 using Microsoft.Xna.Framework;
+using System;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -29,11 +30,21 @@ namespace KeybrandsPlus.Projectiles
         {
             for (int k = 0; k < Main.rand.Next(3, 7); k++)
                 Dust.NewDust(projectile.position + projectile.velocity, projectile.width, projectile.height, ModContent.DustType<Dusts.DraconicFlame>(), projectile.velocity.X * 0.5f, projectile.velocity.Y * 0.5f);
+            for (int k = 0; k < 200; k++)
+            {
+                if (Main.npc[k].active && !Main.npc[k].dontTakeDamage && !Main.npc[k].friendly && Main.npc[k].lifeMax > 5)
+                {
+                    Vector2 vectorTo = Main.npc[k].Center - projectile.Center;
+                    float distanceTo = (float)Math.Sqrt(vectorTo.X * vectorTo.X + vectorTo.Y * vectorTo.Y);
+                    if (distanceTo < 65f)
+                        projectile.Kill();
+                }
+            }
         }
         public override void Kill(int timeLeft)
         {
             Main.PlaySound(SoundID.Item74, projectile.position);
-            Projectile.NewProjectile(projectile.Center.X, projectile.Center.Y, 0f, 0f, ModContent.ProjectileType<DraconicFlareExplosion>(), (int)(projectile.damage * 0.65), projectile.knockBack, projectile.owner);
+            Projectile.NewProjectile(projectile.Center.X, projectile.Center.Y, 0f, 0f, ModContent.ProjectileType<DraconicFlareExplosion>(), (int)(projectile.damage * 0.6), projectile.knockBack, projectile.owner);
             for (int k = 0; k < Main.rand.Next(5, 8); k++)
             {
                 float RandX = Main.rand.NextFloat(3, 5);
@@ -43,7 +54,7 @@ namespace KeybrandsPlus.Projectiles
                 if (Main.rand.NextBool())
                     RandY *= -1;
                 Vector2 RandVelocity = new Vector2(RandX, RandY);
-                Projectile.NewProjectile(projectile.Center, RandVelocity, ModContent.ProjectileType<DraconicFireball>(), (int)(projectile.damage * 0.35), projectile.knockBack / 2, projectile.owner);
+                Projectile.NewProjectile(projectile.Center, RandVelocity, ModContent.ProjectileType<DraconicFireball>(), (int)(projectile.damage * 0.3), projectile.knockBack / 2, projectile.owner);
             }
         }
     }
