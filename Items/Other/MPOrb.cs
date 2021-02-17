664202c8f62556856a7e0f160ffb66d2a4e59a08
Chem <minecartanimation@gmail.com>
2021-02-16 20:44:09 -0700

    Premultiply doesn't work for this apparently, will try again later

--- Items/Other/MPOrb.cs
+++ Items/Other/MPOrb.cs
@@ -19,7 +19,6 @@ namespace KeybrandsPlus.Items.Other
             DisplayName.SetDefault("MP Prize");
             Tooltip.SetDefault("If you somehow got this in your inventory, it's a bug\nPlease let the mod developer know about this\n...Unless you got it via HERO's Mod, Cheat Sheet or the such, you cheater");
         }
-
         public override void SetDefaults()
         {
             item.rare = ItemRarityID.Blue;
@@ -28,7 +27,6 @@ namespace KeybrandsPlus.Items.Other
             Scale = 1f;
             item.maxStack = 100;
         }
-        public override Color? GetAlpha(Color lightColor) => Color.White * .75f;
         public override void Update(ref float gravity, ref float maxFallSpeed)
         {
             if (TimeLeft > 0)
@@ -79,18 +77,16 @@ namespace KeybrandsPlus.Items.Other
         public override bool PreDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, ref float rotation, ref float scale, int whoAmI)
         {
             Texture2D texture = Main.itemTexture[item.type];
-            KeyUtils.PremultiplyTexture(texture);
             Rectangle sourceRectangle = new Rectangle(0, 0, texture.Width, texture.Height);
             Vector2 origin = sourceRectangle.Size() / 2f;
             Color drawColor = item.GetAlpha(lightColor);
-            Main.spriteBatch.Draw(texture, item.Center - Main.screenPosition, sourceRectangle, drawColor, rotation, origin, Scale * Main.essScale, SpriteEffects.None, 0f);
+            Main.spriteBatch.Draw(texture, item.Center - Main.screenPosition, sourceRectangle, Color.White * .75f * Main.essScale, rotation, origin, Scale * Main.essScale, SpriteEffects.None, 0f);
             return false;
         }
         public override bool PreDrawInInventory(SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
         {
             Texture2D texture = Main.itemTexture[item.type];
-            KeyUtils.PremultiplyTexture(texture);
-            spriteBatch.Draw(texture, position, null, Color.White, 0, origin, scale, SpriteEffects.None, 0f);
+            spriteBatch.Draw(texture, position, null, Color.White * .75f, 0, origin, scale, SpriteEffects.None, 0f);
             return false;
         }
     }
