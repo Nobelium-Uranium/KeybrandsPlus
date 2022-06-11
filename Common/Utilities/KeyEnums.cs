9c01c03e0d535e257b1bc21d2cb877fbd481c33a
Chem <minecartanimation@gmail.com>
2022-06-11 10:06:46 -0600

    Enums are cool

---
+++ Common/Utilities/KeyEnums.cs
@@ -0,0 +1,19 @@
+﻿namespace KeybrandsPlus.Common.Utilities
+{
+    internal class KeyEnums
+    {
+        public enum Element
+        { 
+            Mundane,
+            Fire,
+            Blizzard,
+            Thunder,
+            Aero,
+            Water,
+            Light,
+            Dark,
+            Nil,
+            Omnipotent
+        }
+    }
+}
