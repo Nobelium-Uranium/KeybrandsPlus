10a38155da5dc138dfb8388a0d60dc90326fd0ac
ChemAtDark <minecartanimation@gmail.com>
2023-01-27 12:28:51 -0700

    Munny was still a bit too loud

--- Assets/Sounds/KeySoundStyle.cs
+++ Assets/Sounds/KeySoundStyle.cs
@@ -7,24 +7,24 @@ namespace KeybrandsPlus.Assets.Sounds
         #region Other
         public static readonly SoundStyle MunnyBounce = new($"{nameof(KeybrandsPlus)}/Assets/Sounds/Other/MunnyBounce")
         {
-            Volume = .375f,
+            Volume = .25f,
             PitchVariance = .1f
         };
         public static readonly SoundStyle MunnyBounceMed = new($"{nameof(KeybrandsPlus)}/Assets/Sounds/Other/MunnyBounce")
         {
-            Volume = .375f,
-            Pitch = -.1f,
+            Volume = .25f,
+            Pitch = -.075f,
             PitchVariance = .1f
         };
         public static readonly SoundStyle MunnyBounceBig = new($"{nameof(KeybrandsPlus)}/Assets/Sounds/Other/MunnyBounce")
         {
-            Volume = .375f,
-            Pitch = -.2f,
+            Volume = .25f,
+            Pitch = -.15f,
             PitchVariance = .1f
         };
         public static readonly SoundStyle MunnyPickup = new($"{nameof(KeybrandsPlus)}/Assets/Sounds/Other/MunnyPickup")
         {
-            Volume = .125f
+            Volume = .1f
         };
         #endregion
     }
