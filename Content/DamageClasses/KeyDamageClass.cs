d25bc537e81934dd9f7bac3f72f605ef00e0d71f
ChemAtDark <minecartanimation@gmail.com>
2023-08-18 16:20:19 -0600

    Nerf Melee/Ranged/Magic scaling for Universal damage class

--- Content/DamageClasses/KeyDamageClass.cs
+++ Content/DamageClasses/KeyDamageClass.cs
@@ -5,18 +5,14 @@ namespace KeybrandsPlus.Content.DamageClasses
     public class UniversalDamageClass : DamageClass
     {
         internal static UniversalDamageClass Instance;
-        public override void Load()
-        {
-            Instance = this;
-        }
-        public override void Unload()
-        {
-            Instance = null;
-        }
+        public override void Load() => Instance = this;
+        public override void Unload() => Instance = null;
         public override StatInheritanceData GetModifierInheritance(DamageClass damageClass)
         {
-            if (damageClass == Generic || damageClass == Melee || damageClass == Magic || damageClass == Ranged)
+            if (damageClass == Generic)
                 return StatInheritanceData.Full;
+            if (damageClass == Melee || damageClass == Magic || damageClass == Ranged)
+                return new StatInheritanceData(.75f, .75f, .75f, .75f, .75f);
             return StatInheritanceData.None;
         }
         public override bool GetEffectInheritance(DamageClass damageClass)
